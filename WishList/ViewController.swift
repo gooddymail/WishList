77e2d9e81da757a3567537f3130125aad32a6b52
Katchapon Poolpipat <Yowa@Katchapons-MacBook-Pro.local>
2017-01-05 14:51:48 +0700

    initial commit

---
+++ WishList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by Katchapon Poolpipat on 1/5/2560 BE.
+//  Copyright © 2560 Katchapon Poolpipat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
