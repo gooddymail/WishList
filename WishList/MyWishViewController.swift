28ac1188428c3a542c68fbcdb028dd64f9bbdaa1
Katchapon Poolpipat <Yowa@Katchapons-MacBook-Pro.local>
2017-01-08 17:18:27 +0700

    Fix bug when scroll header will move to above add button

--- WishList/MyWishViewController.swift
+++ WishList/MyWishViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+let offsetHeaderStop: CGFloat = 40.0
+let offsetBlackLabelHeader: CGFloat = 95.0
+let distanceWhiteLabelHeader: CGFloat = 35.0
+
 class MyWishViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
@@ -17,29 +21,104 @@ class MyWishViewController: UIViewController {
   @IBOutlet weak var headerImageView: UIImageView!
   @IBOutlet weak var headerBlurImageView: UIImageView!
   @IBOutlet weak var mainNameLabel: UILabel!
-
   
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    configViewElement()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func configViewElement() {
+    avatarImage.layer.cornerRadius = 10.0
+    avatarImage.layer.borderWidth = 3.0
+    avatarImage.layer.borderColor = UIColor.white.cgColor
+    
+    headerImageView.image = UIImage(named: "header_bg")
+    
+    headerBlurImageView.image = UIImage(named: "header_bg")?.blurredImage(withRadius: 10, iterations: 20, tintColor: UIColor.clear)
+    headerBlurImageView.alpha = 0.0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
 
-        // Do any additional setup after loading the view.
-    }
+extension MyWishViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+    
+    return cell
+  }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension MyWishViewController: UIScrollViewDelegate {
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
+    let offset = scrollView.contentOffset.y
+    var avatarTransform = CATransform3DIdentity
+    var headerTransform = CATransform3DIdentity
+    
+    if offset < 0 {
+      let headerScaleFactor: CGFloat = -(offset) / header.bounds.height
+      let headerSizevariation = ((header.bounds.height * (1.0 + headerScaleFactor)) - header.bounds.height)/2.0
+      headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+      headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+      
+      header.layer.transform = headerTransform
+    } else {
+      
+      headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offsetHeaderStop, -offset), 0)
+      
+      let labelTransform = CATransform3DMakeTranslation(0, max(-distanceWhiteLabelHeader, offsetBlackLabelHeader - offset), 0)
+      headerNameLabel.layer.transform = labelTransform
+      
+      headerBlurImageView.alpha = min(1.0, (offset - offsetBlackLabelHeader)/distanceWhiteLabelHeader)
+      
+      let avatarScaleFactor = (min(offsetHeaderStop, offset)) / avatarImage.bounds.height / 1.4
+      let avatarSizeVariation = ((avatarImage.bounds.height * (1.0 + avatarScaleFactor)) - avatarImage.bounds.height) / 2.0
+      avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
+      avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
+      
+      if offset <= offsetHeaderStop {
+        if avatarImage.layer.zPosition < header.layer.zPosition {
+          header.layer.zPosition = 0
+        }
+      } else {
+        if avatarImage.layer.zPosition >= header.layer.zPosition {
+          header.layer.zPosition = 2
+        }
+      }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    header.layer.transform = headerTransform
+    avatarImage.layer.transform = avatarTransform
+  }
 }
